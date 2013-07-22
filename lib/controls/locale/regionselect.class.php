49565148da20983c974ef19bee72896bcfca75cc
Daniel Spors <daniels@scavix.com>
2013-07-22 12:29:02 +0200

    RegionSelect now tries to check if translations are active, using English names instead of text placeholders if not

--- lib/controls/locale/regionselect.class.php
+++ lib/controls/locale/regionselect.class.php
@@ -27,6 +27,7 @@
  */
 namespace WDF\Controls\Locale;
 
+use WDF\Base\Renderable;
 use WDF\Controls\Form\Select;
 use WDF\Localization\CultureInfo;
 use WDF\Localization\Localization;
@@ -73,14 +74,19 @@ class RegionSelect extends Select
 		
 		if( count($regions)>0 )
 		{
+			$cc = current_controller(false);
+			$translations_active = ($cc instanceof Renderable) && $cc->_translate;
 			$sorted = array();
 			foreach($regions as $reg)
 			{
 				if( !$reg ) continue;
 				$code = $reg->Code;
-				$sorted[$code] = array("name"=>tds("TXT_COUNTRY_".strtoupper($code),$reg->EnglishName),"code",$code);
+				if( $translations_active )
+					$sorted[$code] = array("name"=>tds("TXT_COUNTRY_".strtoupper($code),$reg->EnglishName),"code",$code);
+				else
+					$sorted[$code] = array("name"=>$reg->EnglishName,"code",$code);
 			}
-			uasort($sorted, "RegionSelect::compareCountryNames");
+			uasort($sorted, __CLASS__."::compareCountryNames");
 
 			foreach($sorted as $code=>$item)
 				$this->AddOption($code, $item['name']);
