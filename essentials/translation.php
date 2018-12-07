7550d2f962891679ac13412ea6ded07c6b55f14a
Christoph Bünger <christophb@scavix.com>
2018-12-07 09:26:23 +0100

    - added missing jquery image - fixed wdf_unknown_strings_data table creation (with unicode collaction)

--- essentials/translation.php
+++ essentials/translation.php
@@ -245,7 +245,7 @@ function translation_add_unknown_strings($unknown_constants)
 			PRIMARY KEY (term))");
         $ds->ExecuteSql("CREATE TABLE IF NOT EXISTS wdf_unknown_strings_data (
             `term` VARCHAR(150) NOT NULL,
-            `name` VARCHAR(255) NOT NULL,
+            `name` VARCHAR(200) NOT NULL,
             `value` VARCHAR(255) NOT NULL,
             PRIMARY KEY (`term`, `name`))");
 
