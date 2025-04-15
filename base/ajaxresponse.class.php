b666a20024ca832cfd133f4c2331da1f51952b67
Christoph Bünger <christophb@scavix.com>
2025-04-15 10:30:07 +0200

    - cleanup - added "disabled" flag to uiAutocomplete items to show unselectable/disabled items

--- base/ajaxresponse.class.php
+++ base/ajaxresponse.class.php
@@ -38,10 +38,10 @@ class AjaxResponse
 	public $_data = false;
 	public $_text = false;
 	private $_translated = false;
-	
+
 	/**
 	 * This is a valid Noop return.
-	 * 
+	 *
 	 * @param bool $abort_handling Abort clientside success/error handling
 	 * @return AjaxResponse The created response
 	 */
@@ -50,15 +50,15 @@ class AjaxResponse
 		$res = new AjaxResponse();
 		if( $abort_handling )
 		{
-			$res->_data = new stdClass(); 
+			$res->_data = new stdClass();
 			$res->_data->abort = $abort_handling;
 		}
 		return $res;
 	}
-	
+
 	/**
 	 * Return just script code to be executed clientside immetiately.
-	 * 
+	 *
 	 * If $abort_handling is true the clientside success/error handling will not be polled
 	 * @param mixed $script JS code as string or array
 	 * @param bool $abort_handling Abort clientside success/error handling
@@ -71,10 +71,10 @@ class AjaxResponse
 		$data->abort = $abort_handling;
 		return AjaxResponse::Json($data);
 	}
-	
+
 	/**
 	 * Return data JSON formatted ($data can be anything!).
-	 * 
+	 *
 	 * @param mixed $data Data to be passed out
 	 * @return AjaxResponse The created response
 	 */
@@ -85,10 +85,10 @@ class AjaxResponse
 			$res->_data = $data;
 		return $res;
 	}
-	
+
 	/**
 	 * Return a plain text.
-	 * 
+	 *
 	 * @param string $text Text to be passed out
 	 * @return AjaxResponse The created response
 	 */
@@ -99,10 +99,10 @@ class AjaxResponse
 			$res->_text = $text;
 		return $res;
 	}
-	
+
 	/**
 	 * Return a Controller (with full init-code).
-	 * 
+	 *
 	 * @param Renderable $content Content to be passed out
 	 * @param bool $force_dependency_loading If false, stops automatic JS/CSS dependency search and delivery
 	 * @return AjaxResponse The created response
@@ -114,7 +114,7 @@ class AjaxResponse
 		$wrapped->html = $content->WdfRenderAsRoot();
 		if( $content->_translate && system_is_module_loaded('translation') )
 			$wrapped->html = __translate($wrapped->html);
-		
+
         if($force_dependency_loading !== false)
         {
             foreach( Renderable::CategorizeResources(Renderable::__getLazyResources(),$content->__collectResources()) as $r )
@@ -129,10 +129,10 @@ class AjaxResponse
 		$res->_translated = true;
 		return $res;
 	}
-	
+
 	/**
 	 * Return an error.
-	 * 
+	 *
 	 * If $abort_handling is true the clietside error handling will not be polled
 	 * @param string $message The error message
 	 * @param bool $abort_handling Abort clientside success/error handling
@@ -147,10 +147,10 @@ class AjaxResponse
 		$res->_translated = true;
 		return $res;
 	}
-	
+
 	/**
 	 * Let the client redirect.
-	 * 
+	 *
 	 * @param mixed $controller The controller to be loaded (can be <Renderable> or string)
 	 * @param string $event The event to be executed
 	 * @param mixed $data Optional data to be passed (string or array)
@@ -161,17 +161,17 @@ class AjaxResponse
 		$q = buildQuery($controller,$event,$data);
 		return AjaxResponse::Js("wdf.redirect('$q');",true);
 	}
-	
+
 	/**
 	 * Let the client reload the current page.
-	 * 
+	 *
 	 * @return AjaxResponse The created response
 	 */
 	public static function Reload()
 	{
 		return AjaxResponse::Js("document.location.reload();",true);
 	}
-	
+
 	/**
 	 * @internal Renders the response for output.
 	 */
@@ -191,13 +191,12 @@ class AjaxResponse
         }
 		else
 			return '""'; // return an empty string JSON encoded to not kill the app JS side
-//		$res = !$this->_translated&&system_is_module_loaded("translation")? __translate($res):$res;
         return $res;
 	}
-	
+
 	/**
 	 * Allows addition of scripts to responses.
-	 * 
+	 *
 	 * Sometimes it is useful to add JS codes for immediate execution. In plain HTML request this
 	 * would be done with for example <Control::script>() which adds the script to the parent pages init method,
 	 * but for AJAX requests we need to go this way.
@@ -208,12 +207,12 @@ class AjaxResponse
 	{
 		if( !$this->_data )
 			WdfException::Raise("Cannot add script code to AJAX response of type text");
-		
+
 		if( !isset($this->_data->script) )
 			$this->_data->script = force_array($script);
 		else
 			$this->_data->script = array_merge($this->_data->script,force_array($script));
-        
+
         return $this;
 	}
 }
--- res/jquery-ui/ui.autocomplete.ex.js
+++ res/jquery-ui/ui.autocomplete.ex.js
@@ -7,10 +7,13 @@
 			var content = this.options.renderItem
 				?this.options.renderItem.call(this,ul,item)
 				:(item.html?item.html:(item.label?item.label:item.value));
-			return $( "<li></li>" )
-					.data( "item.autocomplete", item )
-					.append($( "<a></a>" ).html(content))
-					.appendTo( ul );
+			var ret = $( "<li></li>" )
+                .data( "item.autocomplete", item )
+                .append($( "<a></a>" ).html(content))
+                .appendTo(ul);
+            if ((typeof (item.disabled) != "undefined") && item.disabled)
+                ret.addClass("ui-state-disabled");
+            return ret;
 		}
 	});
 })(jQuery);
