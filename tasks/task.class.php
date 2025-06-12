f7f39fa6e92f8168b0f3c0d99b0206fc5ea5d9b7
Daniel Spors <daniels@scavix.com>
2025-06-12 14:19:56 +0200

    Added Task timeout handling methods

--- tasks/task.class.php
+++ tasks/task.class.php
@@ -24,6 +24,9 @@
  */
 namespace ScavixWDF\Tasks;
 
+use ScavixWDF\TerminationException;
+use ScavixWDF\Wdf;
+
 /**
  * Represents some work to be done.
  *
@@ -43,7 +46,7 @@ abstract class Task
     /** @var \ScavixWDF\Model\DataSource */
     public $ds;
 
-    function __construct(WdfTaskModel $model=null)
+    function __construct(WdfTaskModel $model = null)
     {
         $this->model = $model;
         $this->ds = \ScavixWDF\Model\DataSource::Get();
@@ -56,7 +59,7 @@ abstract class Task
      *
      * @return static
      */
-    public static function Make() : Task
+    public static function Make(): Task
     {
         $name = get_called_class();
         return new $name();
@@ -68,9 +71,9 @@ abstract class Task
      * @param string $method Optional method name to be started.
      * @return \ScavixWDF\Tasks\WdfTaskModel
      */
-    public static function Async($method='run') : WdfTaskModel
+    public static function Async($method = 'run'): WdfTaskModel
     {
-        $name = get_called_class()."-$method";
+        $name = get_called_class() . "-$method";
         return WdfTaskModel::Create($name);
     }
 
@@ -82,11 +85,11 @@ abstract class Task
      * @param mixed ...$args Optional arguments
      * @return \ScavixWDF\Tasks\WdfTaskModel The new task or a dummy if already present or the one already present
      */
-    public static function AsyncOnce($method='run', $return_original=false, $args = false) : WdfTaskModel
+    public static function AsyncOnce($method = 'run', $return_original = false, $args = false): WdfTaskModel
     {
-        $name = get_called_class()."-$method";
-        if($args)
-            $name .= '-'.md5(serialize($args));
+        $name = get_called_class() . "-$method";
+        if ($args)
+            $name .= '-' . md5(serialize($args));
         return WdfTaskModel::CreateOnce($name, $return_original)->SetArgs($args, !$return_original);
     }
 
@@ -97,12 +100,12 @@ abstract class Task
      * @param array|false $args Optional arguments to check for (default: false)
      * @return bool true if present, else false
      */
-    public static function IsRunning($method='run', $args = false)
+    public static function IsRunning($method = 'run', $args = false)
     {
-        $name = get_called_class()."-$method";
-        if($args)
-            $name .= '-'.md5(serialize($args));
-        return !!WdfTaskModel::Make()->eq('name',$name)->scalar('id');
+        $name = get_called_class() . "-$method";
+        if ($args)
+            $name .= '-' . md5(serialize($args));
+        return !!WdfTaskModel::Make()->eq('name', $name)->scalar('id');
     }
 
     /**
@@ -114,14 +117,14 @@ abstract class Task
      * @param mixed $args Optional arguments given to the task at creation
      * @return bool True if killed, else false
      */
-    public static function Kill($method='run', $args = false)
+    public static function Kill($method = 'run', $args = false)
     {
-        $name = get_called_class()."-$method";
-        if($args)
-            $name .= '-'.md5(serialize($args));
-        if($t = WdfTaskModel::Make()->eq('name',$name)->current())
+        $name = get_called_class() . "-$method";
+        if ($args)
+            $name .= '-' . md5(serialize($args));
+        if ($t = WdfTaskModel::Make()->eq('name', $name)->current())
         {
-            if(avail($t, 'worker_pid'))
+            if (avail($t, 'worker_pid'))
                 WdfTaskModel::Stop($t->worker_pid, false);
             $t->Delete();
             return true;
@@ -138,13 +141,13 @@ abstract class Task
      * @param bool $return_cmdline If true, process will not be started, but it's commandline is returned.
      * @return void|string
      */
-    public function Fork($args=[],$method='run',$return_cmdline=false)
+    public function Fork($args = [], $method = 'run', $return_cmdline = false)
     {
-        if( !function_exists("cli_run_taskprocessor") )
+        if (!function_exists("cli_run_taskprocessor"))
             system_load_module('modules/cli.php');
 
-        array_unshift($args,get_called_class()."-{$method}");
-        return cli_run_script(CLI_SELF,$args,$_SERVER,$return_cmdline);
+        array_unshift($args, get_called_class() . "-{$method}");
+        return cli_run_script(CLI_SELF, $args, $_SERVER, $return_cmdline);
     }
 
     /**
@@ -176,16 +179,16 @@ abstract class Task
     {
     }
 
-    protected function mapCliArgs(&$args,$exact,$names)
+    protected function mapCliArgs(&$args, $exact, $names)
     {
         $ca = count($args);
-        $cn = is_array($names)?count($names):$names;
-        if( $ca<$cn || ($exact && $ca!=$cn) )
-            return array_fill(0,$cn,false);
-        if( !is_array($names) )
-            $names = range(0,$cn-1);
+        $cn = is_array($names) ? count($names) : $names;
+        if ($ca < $cn || ($exact && $ca != $cn))
+            return array_fill(0, $cn, false);
+        if (!is_array($names))
+            $names = range(0, $cn - 1);
         $res = [];
-        foreach( $names as $n )
+        foreach ($names as $n)
             $res[$n] = array_shift($args);
         //log_debug($res);
         return $res;
@@ -193,23 +196,23 @@ abstract class Task
 
     protected function getArg($args, ...$aliases)
     {
-        foreach( $aliases as $a )
-            if( isset($args[$a]) )
+        foreach ($aliases as $a)
+            if (isset($args[$a]))
                 return $args[$a];
         return null;
     }
 
     protected function hasFlag($args, ...$names)
     {
-        foreach( $names as $n )
-            if( in_array($n, $args) )
+        foreach ($names as $n)
+            if (in_array($n, $args))
                 return true;
         return false;
     }
 
     protected function startTerminalMode()
     {
-        $names = array_keys(\ScavixWDF\Wdf::$Logger);
+        $names = array_keys(Wdf::$Logger);
         if (in_array('cli', $names))
             foreach ($names as $n)
                 if ($n != 'cli')
@@ -228,4 +231,51 @@ abstract class Task
         }
         return false;
     }
+
+    #region Timeout handling
+
+    protected $inTransaction = false;
+    protected $terminate_after = false;
+
+    public function preventTimeout()
+    {
+        $this->inTransaction = true;
+        return $this;
+    }
+
+    public function allowTimeout()
+    {
+        $this->inTransaction = false;
+        return $this;
+    }
+
+    protected function SetTerminationTime($end_time)
+    {
+        if (!function_exists('pcntl_async_signals'))
+        {
+            if (PHP_SAPI == 'cli')
+                log_warn(__METHOD__, "pcntl_async_signals doesn't exist, cannot start timeout handling", system_get_caller());
+            return;
+        }
+        $this->terminate_after = $end_time;
+        $ttl = $this->terminate_after - time();
+        Wdf::SetTimeout($ttl, function ()
+        {
+            if ($this->inTransaction)
+                return 1; // retry in 1 second
+
+            if ($this->terminate_after && $this->terminate_after <= time())
+            {
+                $this->terminate_after = false; // disable handling
+                TerminationException::Silent("Max runtime reached");
+            }
+        });
+    }
+
+    protected function SetMaxRuntime($seconds)
+    {
+        $this->SetTerminationTime(time() + $seconds);
+    }
+
+    #endregion
 }
